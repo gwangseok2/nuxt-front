c4f8e17142ad117eac924ec0aec1761edca4fd38
gs.song <rhrnrkd135@gmail.com>
2022-07-12 00:05:49 +0900

    [ + ] 1. 페이지 네이션 제작중

---
+++ components/common_pagination.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+
+        data() {
+            return {
+
+                // 현재 페이지
+                thisPageNum : 1,
+
+                // 한 페이지에 보여줄 데이터
+                onePageData: 16,
+                
+                // 빈 배열
+                commentlist : [],
+                
+            }
+        },
+
+        computed: {
+            startPage() {
+                return ((this.thisPageNum - 1) * this.onePageData);
+            },
+
+            endPage() {
+                return (this.startPage + this.onePageData);
+            }
+        }
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
