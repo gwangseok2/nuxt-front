01dd912c41259baec3e4e68a71f702b0e7add627
gs.song <rhrnrkd135@gmail.com>
2022-09-14 19:40:47 +0900

    [ + ] BASE_URL 수정

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const instance = axios.create({
   // baseURL: process.env.BASE_URL,
 
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.BASE_URL,
 })
 function fetchProductById(id) {
   return instance.get(`/products/${id}`)
