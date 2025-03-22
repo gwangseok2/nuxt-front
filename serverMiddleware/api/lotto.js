73d80539fcd9e7fbb8ddaf07a0879d395fd3a80a
Sging <105583538+gwangseok2@users.noreply.github.com>
2025-03-23 01:16:24 +0900

    [ + - ] debug

--- serverMiddleware/api/lotto.js
+++ serverMiddleware/api/lotto.js
@@ -4,6 +4,13 @@ const { parse } = require('url')
 module.exports = async (req, res) => {
   const { drwNo } = parse(req.url, true).query
 
+  if (!drwNo) {
+    res.statusCode = 400
+    return res.end(
+      JSON.stringify({ error: 'drwNo 쿼리 파라미터가 필요합니다.' })
+    )
+  }
+
   try {
     const response = await axios.get('https://www.dhlottery.co.kr/common.do', {
       params: {
@@ -17,7 +24,10 @@ module.exports = async (req, res) => {
   } catch (error) {
     res.statusCode = 500
     res.end(
-      JSON.stringify({ error: '로또 API 호출 실패', message: error.message })
+      JSON.stringify({
+        error: '로또 API 호출 실패',
+        message: error.message,
+      })
     )
   }
 }
